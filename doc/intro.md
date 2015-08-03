b56460dfb948b6f8269247b90ffb0ca66ecf7d1a
Rui Guo <rguo@leancloud.rocks>
2015-08-03 23:26:39 +0800

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to lean-parrot
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/lean_parrot/core.clj
@@ -0,0 +1,7 @@
+(ns lean-parrot.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
